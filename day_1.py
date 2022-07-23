256ecd77ca74eb543861006d55378a82eb4c2942
leadtech <leadtech@localhost.localdomain>
2022-07-23 21:22:43 +0530

    day_1

---
+++ day_1.py
@@ -0,0 +1,42 @@
+
+
+# print("Hello World!")
+
+integer=20
+float_value=4.5
+
+# add_value=integer + float_value
+# sub_value=integer - float_value
+# multiply_value=integer * float_value
+# divide_value=integer / float_value
+# # modulo_value=integer ** float_value  # (10)5
+# modulo_value=2 ** 3  # (10)5
+# print(add_value)
+# print(sub_value)
+# print(multiply_value)
+# print(divide_value)
+# print(modulo_value)
+
+# print(type(integer))
+# float_val_con=float(integer)
+# print(float_val_con)
+# print(type(float_val_con))
+
+# str_float=str(float_val_con)
+# print(type(str_float))
+
+# $abc="djhjksj" # this will not work
+
+
+text="Hello Guys, Plz Subscribe my Channel And share with your friends     "
+text_2="Thank You for watching"
+
+merge_str=text + text_2
+
+# print(merge_str)
+# print("your" in text)
+# print(text.find('my'))
+# print(text.upper())
+print(text.capitalize())
+print(text[5:])
+print(text.strip())
