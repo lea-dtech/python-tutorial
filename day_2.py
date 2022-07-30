62c25b6fe9e90391f0c30b61a23405810bbb3a2b
vikram <vikramkumar8655@gmail.com>
2022-07-30 14:10:56 +0530

    day_2

---
+++ day_2.py
@@ -0,0 +1,17 @@
+
+# (5==5) -->True (equal operator)
+# > grether than operator
+# < smaller than 
+# >= greather than equal to
+# <= smaller than equal to
+# ! not operator example:-[ (!(1==1)) it will return False ]
+
+num_1=11
+num_2=11
+
+if (num_1<num_2):
+    print("num_1 is smaller than num_2.")
+elif (num_1>num_2):
+    print("num_1 is grether than num_2.")
+else:
+    print("both number is equal.")
