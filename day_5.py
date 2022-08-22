7e90cf7f1ce6ef242ea808be6cd078debc91eaa4
vikram <vikramkumar8655@gmail.com>
2022-08-22 19:38:52 +0530

    day_5

---
+++ day_5.py
@@ -0,0 +1,19 @@
+# For and While Loop
+
+list_1=[1,2,3,4,5,6,7,89,11,12,13,13,10,1,2,3,55]
+
+i=0
+# for integer in list_1:
+#     print("loop", i, integer)
+#     i+=1
+
+while i<10:
+    if i==6:
+        i+=1
+        continue
+    print(i)
+    i+=1
+    # if i==6:
+    #     break
+
+    
