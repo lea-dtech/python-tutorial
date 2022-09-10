25a723ad15913a2c304dbca211d1a36d99ca1541
vikram <vikramkumar8655@gmail.com>
2022-09-10 19:25:31 +0530

    day_6

---
+++ day_6.py
@@ -0,0 +1,10 @@
+
+list_1=[1,"Rohan","123",12.5,False,"item",'Mohan',"Abhay"]
+
+list_1[3]="Shyam"
+list_2=[1,2,3,4,5]
+print(list_1[2])
+print(list_1[2:])
+print(list_1[:5])
+# print(list_1.index("sdghgha"))  #it will throw error
+print(list_1+list_2)
