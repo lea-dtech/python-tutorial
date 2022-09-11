bab28cbf56e46c41b8df88fa27bce40f6e00212e
vikram <vikramkumar8655@gmail.com>
2022-09-11 21:49:02 +0530

    day_7

---
+++ day_7.py
@@ -0,0 +1,12 @@
+
+variable_tuple=(1,2,3,4,5,"aagds")
+# variable_tuple[0]="5"  # it will throw error
+# print(variable_tuple)
+
+# list tutorial code
+list_1=[1,5,6]
+
+list_1.append(3)
+list_1.pop(0)
+list_1.insert(1, "Ram")
+print(list_1)
